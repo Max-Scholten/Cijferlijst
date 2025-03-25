465e00f956f66c643b68d82e9999723f2afb180c
Max-Scholten <maxscholten4002@gmail.com>
2025-03-25 14:52:11 +0100

    New Axios

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+createApp(App).use(VueAxios, axios).mount('#app1    ')
 createApp(App).use(router).mount('#app')
